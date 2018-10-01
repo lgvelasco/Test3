bbc28fe3b4e4ab292bce5a4493d81ec39d3c5362
luisguillermo <guillermo.velasco98@gmail.com>
2018-10-01 20:08:38 +0200

    Fixed bug

--- Homework.py
+++ Homework.py
@@ -6,12 +6,14 @@ submax = ""
 
 for i in range(0, len(s)-1):
 
-    if s[i] < s[i+1]:
-        sub = sub + s[i] + s[i+1]
+    if s[i] <= s[i+1]:
+        if sub == "":
+            sub = s[i] + s[i+1]
+        else:
+            sub = sub + s[i+1]
     elif s[i] > s[i+1]:
         sub = ""
 
     if len(sub) > len(submax):
         submax = sub
-
 print(submax)
